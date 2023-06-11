ea2235bac0bf70ce6e8754b28d7ce2e44d0bd04e
johndy <johndypanca12345@gmail.com>
2023-06-11 21:19:25 +0700

    update video

--- app/src/main/java/com/example/uts_akb_10120905/ui/music/MusicFragment.kt
+++ app/src/main/java/com/example/uts_akb_10120905/ui/music/MusicFragment.kt
@@ -1,16 +1,20 @@
 package com.example.uts_akb_10120905.ui.music
 
 import MusicAdapter
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
+import android.widget.MediaController
+import android.widget.VideoView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.uts_akb_10120905.R
 
-
 class MusicFragment : Fragment(R.layout.fragment_music) {
 
     private val musicAdapter = MusicAdapter()
+
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -26,8 +30,27 @@ class MusicFragment : Fragment(R.layout.fragment_music) {
             MusicModel(R.drawable.albumpic3, "Out of Time", "The Weeknd",R.raw.outoftime),
         )
         val rvmusic = getView()?.findViewById<RecyclerView>(R.id.rvmusic)
+        val videoView = getView()?.findViewById<VideoView>(R.id.videoView)
+        val videoPath = "android.resource://" + requireActivity().packageName + "/" + R.raw.video1
+
+        val uri = Uri.parse(videoPath)
+        videoView?.setVideoURI(uri)
+
+        val mediaController = MediaController(requireContext())
+        videoView?.setMediaController(mediaController)
+        mediaController.setAnchorView(videoView)
+        mediaController.requestFocus()
+
+        val imageButton = getView()?.findViewById<ImageButton>(R.id.play_button)
+        imageButton?.setOnClickListener {
+            if (!videoView?.isPlaying!!) {
+                videoView.start()
+                imageButton.visibility = View.GONE
+            }
+        }
         rvmusic?.adapter = musicAdapter
         musicAdapter.differ.submitList(music)
+
 
     }
 }
